2a427f8716663b09c017d9f032b6eda50fc7b0d5
Pratistha Chaira <thepratisthachaira@gmail.com>
2026-04-15 21:23:28 +0530

    Create UC5.java

---
+++ TrainConsistManagementApp/UC5.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TrainApp {
+
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+
+        Set<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+
+        trainFormation.add("Sleeper");
+
+        System.out.println("Final Train Formation (Ordered & Unique): " + trainFormation);
+    }
+}
